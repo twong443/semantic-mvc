6d7bf147dd1f5fcdd64593f80475938503018a5b
Tiffany Wong <t.wong.443@gmail.com>
2017-12-20 23:04:34 -0500

    Add project files.

---
+++ WebApplication1/Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            var random = new Random(999);
+
+            var items = new List<string>();
+            for (int i = 1; i < 50; ++i)
+                items.Add($"{random.Next()}");
+
+            ViewBag.Items = items;
+            return View();
+        }   
+    }       
+}           
